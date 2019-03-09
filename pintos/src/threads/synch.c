add269e7ef3925dbac5dbc6e1f0dc9e3acc538e4
JaeJunLee <cooco119@gmail.com>
2019-03-09 23:33:28 +0900

    Pass priority-sema using busy waiting

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -69,7 +69,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, &compare_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -114,10 +114,14 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
   sema->value++;
+  if (sema->value > 0){
+    if (!list_empty (&sema->waiters)) 
+      thread_unblock (list_entry (list_pop_front (&sema->waiters),
+                                  struct thread, elem));
+      if (thread_current () != idle_thread)
+        thread_yield ();
+  }
   intr_set_level (old_level);
 }
 
