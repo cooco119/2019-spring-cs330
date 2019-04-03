c1795185bf608f5e0998f3adaf7fd25c6dcc4971
JaeJunLee <cooco119@gmail.com>
2019-04-04 00:38:19 +0900

    Pass sc-*

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,12 +3,15 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
 void halt (void);
 int exit (int status);
+tid_t exec (const char *cmd_line);
 int wait (tid_t tid);
+int read (int fd, void *buffer, unsigned size);
 int write (int fd, const void *buffer, unsigned size);
 
 void
@@ -22,16 +25,27 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   // printf ("system call!\n");
   // printf ("syscal num: %d\n", *(uint32_t*) (f->esp));
+  
   switch (*(uint32_t *)(f->esp)) {
     case SYS_HALT:                   /* Halt the operating system. */
       halt ();
       break;
     case SYS_EXIT:                   /* Terminate this process. */
+      if (! is_user_vaddr(f->esp + 4)){
+        exit(-1);
+      }
       exit(*(uint32_t*) (f->esp + 4));
       break;
     case SYS_EXEC:                   /* Start another process. */
+      if (! is_user_vaddr(f->esp + 4)){
+        exit(-1);
+      }
+      exec(*(uint32_t*) (f->esp + 4));
       break;
     case SYS_WAIT:                   /* Wait for a child process to die. */
+      if (! is_user_vaddr(f->esp + 4)){
+        exit(-1);
+      }
       wait( (tid_t)*(uint32_t*)(f->esp + 4));
       break;
     case SYS_CREATE:                 /* Create a file. */
@@ -43,8 +57,16 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_FILESIZE:               /* Obtain a file's size. */
       break;
     case SYS_READ:                   /* Read from a file. */
+      if (! is_user_vaddr(f->esp + 4) || !is_user_vaddr(f->esp + 8) || !is_user_vaddr(f->esp + 12)){
+        exit(-1);
+      }
+      read ((int) *(uint32_t*) (f->esp + 4), (void *) *(uint32_t*) (f->esp + 8),
+            (unsigned) *(uint32_t*) (f->esp + 12));
       break;
     case SYS_WRITE:                  /* Write to a file. */
+      if (! is_user_vaddr(f->esp + 4) || !is_user_vaddr(f->esp + 8) || !is_user_vaddr(f->esp + 12)){
+        exit(-1);
+      }
       write ((int) *(uint32_t*) (f->esp + 4), (void *) *(uint32_t*) (f->esp + 8), 
               (unsigned) *(uint32_t*) (f->esp + 12));
       break;
@@ -69,8 +91,29 @@ int exit(int status) {
   thread_exit();
 }
 
+tid_t exec (const char *cmd_line) {
+  return process_execute(cmd_line);
+}
+
 int wait (tid_t tid) {
   return process_wait(tid);
+}
+
+int read (int fd, void *buffer, unsigned size) {
+  int i;
+  int STDIN = 0;
+
+  if (fd == STDIN) {
+    char *tmp = malloc(sizeof(char) * size);
+    for (i = 0; i < size; i++) {
+      tmp[i] = (char) input_getc();
+      if (tmp[i] == '\0') {
+        break;
+      }
+    }
+    buffer = (void *) tmp;
+    return i;
+  }
 }
 
 int write (int fd, const void *buffer, unsigned size) {
